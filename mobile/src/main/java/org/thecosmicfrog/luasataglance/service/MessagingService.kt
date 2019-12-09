a18633a197970045f6c46783146ca061c06b2645
Aaron Hastings <aaron@aaronhastings.me>
2019-12-09 20:57:06 +0000

    [Trivial] Fixed some minor comment issues.

--- mobile/src/main/java/org/thecosmicfrog/luasataglance/service/MessagingService.kt
+++ mobile/src/main/java/org/thecosmicfrog/luasataglance/service/MessagingService.kt
@@ -16,7 +16,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Luas at a Glance.  If not, see <http:></http:>//www.gnu.org/licenses/>.
+ * along with Luas at a Glance.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.thecosmicfrog.luasataglance.service
 
